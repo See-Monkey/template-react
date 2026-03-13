bc90e67fc77075eb98ae3b186779dccbc320e8e8
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-12 19:45:39 -0600

    Fix relative paths

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router";
-import App from "./App.jsx";
+import App from "../App.jsx";
 
-import Home from "./pages/Home/Home.jsx";
-import Login from "./pages/Login/Login.jsx";
-import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import Home from "../pages/Home/Home.jsx";
+import Login from "../pages/Login/Login.jsx";
+import ErrorPage from "../pages/ErrorPage/ErrorPage.jsx";
 
 const router = createBrowserRouter([
 	{
